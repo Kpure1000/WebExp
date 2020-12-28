1582a92993b643d1c86caa8b96237e6cfc9bf624
Kpure1000 <ktsq2000@126.com>
2020-12-28 18:13:21 +0800

    update:change some style and add click event

--- js/login_shift.js
+++ js/login_shift.js
@@ -1,3 +1,12 @@
- function test1(params) {
-    
+var withName = document.getElementById("with_name");
+var withPhone = document.getElementById("with_phone");
+
+function showBlock(i) {
+    if (i == 1) {
+        withName.style.display = "none";
+        withPhone.style.display = "block";
+    } else if (i == 2) {
+        withName.style.display = "block";
+        withPhone.style.display = "none";
+    }
 }
--- php/login.php
+++ php/login.php
@@ -4,27 +4,18 @@
     $password = $_POST['password'];
     $phonenum = $_POST['phonenumber'];
     $feedback = $_POST['feedback'];
-    // $name_sub = $_POST['name_submit'];
-    // $msg_sub = $_POST['msg_submit'];
     $conn = new mysqli('localhost','root',null,'login');
     if ($conn->connect_error){
         echo '数据库连接失败！';
         exit(0);
     }else{
-        // if ($username == ''){
-        //     echo '<script>alert("请输入用户名！");history.go(-1);</script>';
-        //     exit(0);
-        // }
-        // if ($password == ''){
-        //     echo '<script>alert("请输入密码！");history.go(-1);</script>';
-        //     exit(0);
-        // }
         if($username!='' || $password!=''){
             //  TODO: loing with user name
-            $sql = "select name,pass from user where name = '$_POST[username]' and pass = '$_POST[password]'";
+            $sql = "select name,pass from user where name = '$username' and pass = '$password'";
         }
-        if($phonenum!='' || $feedback!=''){
+        else if($phonenum!='' || $feedback!=''){
             //  TODO: login with phone number
+            $sql = "select name,pass from user where name = '$_POST[username]' and pass = '$_POST[password]'";
         }
         else {
             echo '<script>alert("请输入！");history.go(-1);</script>';
@@ -38,4 +29,3 @@
             echo '<script>alert("用户名或密码错误！");history.go(-1);</script>';
         }
     }
-?>
