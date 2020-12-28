ec6082b007c1c540d7f5e533d7e5ae8602edfe6c
Kpure1000 <ktsq2000@126.com>
2020-12-28 23:47:05 +0800

    update: check in js

--- js/login_shift.js
+++ js/login_shift.js
@@ -1,12 +1,49 @@
-var withName = document.getElementById("with_name");
-var withPhone = document.getElementById("with_phone");
-
+function $(id) {
+    return document.getElementById(id);
+}
+var isWithName = true;
+window.onload = function () {
+    $("username").value=null;
+    $("password").value=null;
+    $("phonenumber").value=null;
+    $("checkingcode").value=null;
+}
 function showBlock(i) {
     if (i == 1) {
-        withName.style.display = "none";
-        withPhone.style.display = "block";
+        isWithName = false;
+        var uname = $("username");
+        var pass = $("password");
+        uname.value = null;
+        pass.value = null;
+        $("with_name").style.display = "none";
+        $("with_phone").style.display = "block";
     } else if (i == 2) {
-        withName.style.display = "block";
-        withPhone.style.display = "none";
+        isWithName = true;
+        var phone = $("phonenumber");
+        var check = $("checkingcode");
+        phone.value = null;
+        check.value = null;
+        $("with_name").style.display = "block";
+        $("with_phone").style.display = "none";
+    }
+}
+
+function checkSubmit() {
+    if (isWithName) {
+        var uname = $("username");
+        var pass = $("password");
+        if (uname.value.length > 30 || pass.value.length < 6 || pass.value.length > 20) {
+            alert("Lenght Wrong");
+            return false;
+        }
+        return true;
+    } else {
+        var phone = $("phonenumber");
+        var check = $("checkingcode");
+        if (phone.value.length > 11 || check.value.length > 4) {
+            alert("Phone Wrong");
+            return false;
+        }
+        return true;
     }
 }
--- php/login.php
+++ php/login.php
@@ -1,31 +1,47 @@
 <?php
-    header("Content-type: text/html; charset=utf-8");
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $phonenum = $_POST['phonenumber'];
-    $feedback = $_POST['feedback'];
-    $conn = new mysqli('localhost','root',null,'login');
-    if ($conn->connect_error){
-        echo '数据库连接失败！';
-        exit(0);
-    }else{
-        if($username!='' || $password!=''){
-            //  TODO: loing with user name
-            $sql = "select name,pass from user where name = '$username' and pass = '$password'";
+header("Content-type: text/html; charset=utf-8");
+$username = $_POST['username'];
+$password = $_POST['password'];
+$phonenum = $_POST['phonenumber'];
+$feedback = $_POST['feedback'];
+$conn = new mysqli('localhost', 'root', null, 'login');
+if ($conn->connect_error) {
+    echo 'Database connect failed';
+    exit(0);
+} else {
+    if ($username != '' || $password != '') {
+        if (strlen($username) > 30) {
+            echo '<script>alert("Username length wrong");history.go(-1);</script>';
         }
-        else if($phonenum!='' || $feedback!=''){
-            //  TODO: login with phone number
-            $sql = "select name,pass from user where name = '$_POST[username]' and pass = '$_POST[password]'";
+        if (strlen($password) < 6 || strlen($password) > 20) {
+            echo '<script>alert("Password length wrong");history.go(-1);</script>';
         }
-        else {
-            echo '<script>alert("请输入！");history.go(-1);</script>';
-            exit(0);
-        }
+        $sql = "select name,pass from user where name = '$username' and pass = '$password'";
         $result = $conn->query($sql);
         $number = mysqli_num_rows($result);
         if ($number) {
             echo '<script>window.location="../index.html";</script>';
         } else {
-            echo '<script>alert("用户名或密码错误！");history.go(-1);</script>';
+            echo '<script>alert("Username or password wrong or not exist");history.go(-1);</script>';
         }
+    } else if ($phonenum != '' || $feedback != '') {
+        if (strlen($phonenum) > 22) {
+            echo '<script>alert("Phone number length wrong");history.go(-1);</script>';
+        }
+        if (strlen($feedback) > 4) {
+            echo '<script>alert("Check code length wrong");history.go(-1);</script>';
+        }
+        $sql = "select phone from user where phone = '$_POST[phonenum]'";
+        $result = $conn->query($sql);
+        $number = mysqli_num_rows($result);
+        if ($number) {
+            //  TODO : check the feedback
+        } else {
+            echo '<script>alert("This phone number did not be registed");history.go(-1);</script>';
+        }
+        echo '<script>alert("Check code wrong");history.go(-1);</script>';
+    } else {
+        echo '<script>alert("Please input");history.go(-1);</script>';
+        exit(0);
     }
+}
