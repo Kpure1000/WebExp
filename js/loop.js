36f76d33c7e7a449647e6171e26e94ace09d433a
Kpure1000 <ktsq2000@126.com>
2020-12-28 16:22:05 +0800

    add:login way with msg and js

---
+++ js/loop.js
@@ -0,0 +1,6 @@
+var num = 0;
+var imgs = []
+
+function changeImg(params) {
+    
+}
--- php/login.php
+++ php/login.php
@@ -2,20 +2,34 @@
     header("Content-type: text/html; charset=utf-8");
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $phonenum = $_POST['phonenumber'];
+    $feedback = $_POST['feedback'];
+    // $name_sub = $_POST['name_submit'];
+    // $msg_sub = $_POST['msg_submit'];
     $conn = new mysqli('localhost','root',null,'login');
     if ($conn->connect_error){
         echo '数据库连接失败！';
         exit(0);
     }else{
-        if ($username == ''){
-            echo '<script>alert("请输入用户名！");history.go(-1);</script>';
+        // if ($username == ''){
+        //     echo '<script>alert("请输入用户名！");history.go(-1);</script>';
+        //     exit(0);
+        // }
+        // if ($password == ''){
+        //     echo '<script>alert("请输入密码！");history.go(-1);</script>';
+        //     exit(0);
+        // }
+        if($username!='' || $password!=''){
+            //  TODO: loing with user name
+            $sql = "select name,pass from user where name = '$_POST[username]' and pass = '$_POST[password]'";
+        }
+        if($phonenum!='' || $feedback!=''){
+            //  TODO: login with phone number
+        }
+        else {
+            echo '<script>alert("请输入！");history.go(-1);</script>';
             exit(0);
         }
-        if ($password == ''){
-            echo '<script>alert("请输入密码！");history.go(-1);</script>';
-            exit(0);
-        }
-        $sql = "select name,pass from user where name = '$_POST[username]' and pass = '$_POST[password]'";
         $result = $conn->query($sql);
         $number = mysqli_num_rows($result);
         if ($number) {
